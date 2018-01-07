112aa7163cfa6b5a6e24af0707cc8ec7c81df28c
corianderHunter <11@qq.com>
2018-01-07 15:56:25 +0800

    shalter edit

---
+++ src/pages/shalter/editShalter..vue
@@ -0,0 +1,18 @@
+<template>
+    <div class="editShalter">
+      <i class="el-icon-close close"></i>
+    </div>
+</template>
+
+<script>
+    export default{}
+</script>
+
+<style rel="stylesheet/scss" lang="scss" scoped>
+  .editShalter{
+    position: absolute;
+    .close{
+      position: absolute;
+    }
+  }
+</style>
--- src/pages/shalter/list.vue
+++ src/pages/shalter/list.vue
@@ -6,6 +6,7 @@
           <el-input
             v-model="search.name">
             <i slot="prefix" class="el-input__icon el-icon-search"></i>
+            <i slot="suffix" class="el-input__icon el-icon-close cp" @click="clearSearch" v-show="closeShow"></i>
           </el-input>
         </div>
         <el-table
@@ -20,19 +21,37 @@
             label="大棚名">
           </el-table-column>
           <el-table-column
-            prop="id"
+            prop="gateway"
             label="网关序列号">
           </el-table-column>
           <el-table-column
             width="100"
             >
             <template slot-scope="scope">
-              <i class="el-icon-edit cp"></i>
+              <i class="el-icon-edit cp" @click="edit(scope.row.id)"></i>
               <i class="el-icon-view cp"></i>
             </template>
           </el-table-column>
         </el-table>
       </div>
+      <el-dialog :visible.sync="editVisible" class="shalter-edit-dialog">
+        <div slot="title" class="title">大棚修改</div>
+        <el-form :model="editForm">
+          <el-form-item label="大棚名" :label-width="labelWidth">
+            <el-input v-model="editForm.name"></el-input>
+          </el-form-item>
+          <el-form-item label="网关序列号" :label-width="labelWidth">
+            <el-select v-model="editForm.region" placeholder="选择网关序列号" style="width:100%;">
+              <el-option label="111111" value="1"></el-option>
+              <el-option label="222222" value="2"></el-option>
+            </el-select>
+          </el-form-item>
+        </el-form>
+        <div slot="footer" class="dialog-footer">
+          <el-button @click="editVisible = false">取 消</el-button>
+          <el-button type="primary" @click="editVisible = false">确 定</el-button>
+          </div>
+      </el-dialog>
     </div>
 </template>
 
@@ -41,8 +60,15 @@
     export default{
       data(){
         return {
+          editVisible:false,
+          labelWidth:'120px',
           search:{
             name:''
+          },
+          editForm:{
+            id:'',
+            name:'',
+            gateway:'',
           },
           overview:{
             title:'大棚温室管理',
@@ -50,12 +76,14 @@
           },
           list:[
             {
+              id:1,
               name:'草莓大棚',
-              id:'12342424'
+              gateway:'12342424'
             },
             {
+              id:2,
               name:'葡萄玻璃温室',
-              id:'12342424'
+              gateway:'12342424'
             },
           ]
         }
@@ -65,24 +93,58 @@
           return this.list.filter(val => {
               return val.name.indexOf(this.search.name)>-1;
             });
+        },
+        closeShow(){
+          return !!this.search.name||(this.search.name === 0)
         }
       },
       components:{
         Header,
       },
+      methods:{
+        edit(id){
+          this.editVisible = true;
+          this.editForm.id = id;
+        },
+        clearSearch(){
+          this.search.name = ''
+        }
+      }
     }
 </script>
 
 <style rel="stylesheet/scss" lang="scss">
+  .shalter-edit-dialog{
+    .el-dialog{
+      width: 30rem;
+      border: 2px solid #7eb338;
+      .el-dialog__body{
+        padding-left: 2rem;
+        padding-right: 2rem;
+        .el-form-item__label{
+          font-size: 1rem;
+        }
+      }
+      .el-button{
+        font-size: 1rem;
+      }
+    }
+    .title{
+      text-align: center;
+      font-size: 1.2rem;
+    }
+  }
   .shalter-list-container{
     padding: 1.5rem;
     .util-bar{
       border-left:1px solid #e0e0e0;
       border-top:1px solid #e0e0e0;
       border-right:1px solid #e0e0e0;
-      padding-left:30px;
+      padding-left:1.5rem;
       .el-input{
         font-size:1rem;
+        padding-left:1rem;
+        width:15rem;
         input{
           border: none;
         }
@@ -96,12 +158,12 @@
       th{
         background-color: #f7f7f7;
         &:nth-child(1){
-          padding-left:30px;
+          padding-left:1.5rem;
          }
       }
       td{
         &:nth-child(1){
-           padding-left:30px;
+           padding-left:1.5rem;
          }
       }
     }
